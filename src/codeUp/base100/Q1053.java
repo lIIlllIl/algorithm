a02cf575bee1a10fbb473fafecce9a9762417364
MIN <backup1237@naver.com>
2020-05-03 15:43:23 +0900

    2020-05-03 commit

---
+++ src/codeUp/base100/Q1053.java
@@ -0,0 +1,18 @@
+package codeUp.base100;
+
+import java.util.Scanner;
+
+public class Q1053 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        
+        if(a == 0) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println(0);
+        }
+    }
+}
---
+++ src/codeUp/base100/Q1069.java
@@ -0,0 +1,28 @@
+package codeUp.base100;
+
+import java.util.Scanner;
+
+public class Q1069 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String s = sc.nextLine();
+        
+        switch(s) {
+            case "A" : 
+                System.out.println("best!!!");
+                break;
+            case "B" : 
+                System.out.println("good!!");
+                break;
+            case "C" : 
+                System.out.println("run!");
+                break;
+            case "D" : 
+                System.out.println("slowly~");
+                break;
+            default : 
+                System.out.println("what?");
+        }
+    }
+}
